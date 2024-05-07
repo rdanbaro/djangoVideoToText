a957cc5609999aabb1439b1eb04aeff9b7de72a5
fabroche <kraxusmmo@gmail.com>
2024-05-07 16:16:57 -0400

    preparando los estilos del proyecto

--- djangoVideoToText/settings.py
+++ djangoVideoToText/settings.py
@@ -115,12 +115,16 @@ STATIC_URL = 'static/'
 
 PROYECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
-STATICFILES_DIRS = [
-    os.path.join(PROYECT_ROOT, '..', 'static')
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    MEDIA_ROOT,
+]
+
+STATIC_ROOT = BASE_DIR / 'static-root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
