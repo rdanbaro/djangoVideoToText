c5c633d5b331547b933be074ae0f204504277d01
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-05-07 14:58:31 -0400

    url ficheros ok, comentarios eliminados

--- videototext/views.py
+++ videototext/views.py
@@ -23,12 +23,10 @@ def upload_file(request):
 
         # transcribiendo el archivo
         model = whisper.load_model("base")
-        # result = model.transcribe(f'/home/gresuto/Ebe VideoToText/djangoVideoToText/{uploaded_file_url}')
-        result = model.transcribe(settings.BASE_DIR / uploaded_file_url)
+        result = model.transcribe(f'{settings.BASE_DIR}{uploaded_file_url}')
 
         return render(request, 'upload-file.html', {
             'uploaded_file_url': uploaded_file_url,
-            'transcription': result["text"],
         })
 
     return render(request, 'upload-file.html')
