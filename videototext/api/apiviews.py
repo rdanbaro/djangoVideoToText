799c9f48275f608c804fa199499e917ae663bcb0
fabroche <kraxusmmo@gmail.com>
2024-05-13 18:51:19 -0400

    fixed no transcribia porque estaba comentariado para cuando era un video

--- videototext/api/apiviews.py
+++ videototext/api/apiviews.py
@@ -57,7 +57,7 @@ class TransciptionApiView(APIView):
                 # # start
                 if es_video(file):
                     archivo_inst = conversion(file)
-                    # archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
+                    archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
                 else:
                     archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
                     archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
