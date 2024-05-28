25a65160719f48be72d5acd2787d9df4d42e764f
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-05-28 19:04:05 -0400

    fixing config server whitenoise

--- djangoVideoToText/settings.py
+++ djangoVideoToText/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     # My Apps
     'rest_framework',
     'drf_yasg',
