56ebdc5905b922364ce61caf09a375ec90a0aff7
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-05-29 07:35:59 -0400

    actualizando api link

--- videototext/utils/utils.py
+++ videototext/utils/utils.py
@@ -8,8 +8,12 @@ def read_config(endpoint=None):
     config.read(settings.BASE_DIR / 'videototext/config.ini')
 
     # Lee los valores de las secciones
-    host = config.get('HOST', 'hostname')
-    port = config.getint('PORT', 'port_number')
+    #host = config.get('HOST', 'hostname')
+    #port = config.getint('PORT', 'port_number')
+    
+    host = 'https://djangovideototext-production.up.railway.app/'
+    port = '6187'
+    
     api_url = config.get('URLS', endpoint if endpoint else 'transcription_app')
 
     if endpoint:
