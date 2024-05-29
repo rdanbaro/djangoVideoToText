b8443148437ba9e2c6456d915df8951f70310f4e
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-05-29 06:50:12 -0400

    add postgre database

--- djangoVideoToText/settings.py
+++ djangoVideoToText/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'djangoVideoToText.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:ojzYHRkHyFlvAOXGWztQStxrpDPhrNDr@roundhouse.proxy.rlwy.net:29696/railway')
 }
 
 # Password validation
@@ -138,3 +136,5 @@ STATIC_ROOT = BASE_DIR / 'static-root'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://djangovideototext-production.up.railway.app']
