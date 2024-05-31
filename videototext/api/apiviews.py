3ffda970c0667ed31bc23064b6cb7733ac86d10a
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-05-31 07:35:24 -0400

    arreglando identacion en produccion

--- videototext/api/apiviews.py
+++ videototext/api/apiviews.py
@@ -59,6 +59,7 @@ class TransciptionApiView(APIView):
                 if es_video(file):
                     #archivo_inst = conversion(file)
                     #archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
+                    print('hola')
                 elif es_audio(file):
                     archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
                     #archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
