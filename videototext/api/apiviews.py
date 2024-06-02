0e3f33f04a099afc039ef3cb6e5adac1351a5209
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-06-02 12:40:59 -0400

    probando transcripcion en produccion

--- videototext/api/apiviews.py
+++ videototext/api/apiviews.py
@@ -56,18 +56,18 @@ class TransciptionApiView(APIView):
                 # # validando que sea un audio y transcribiendolo,
                 # # if esVideo se convierte y luego se transcribe else se transcribe directamente
                 # # start
-               # if es_video(file):
-               #     #archivo_inst = conversion(file)
-               #     #archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
-               #     print('hola')
-               # elif es_audio(file):
-               #     archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
-               #     #archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
-               # else:
-               #     archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
-               #     archivo_inst.url = None    
+                if es_video(file):
+                    archivo_inst = conversion(file)
+                    archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
+                    
+                elif es_audio(file):
+                    archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
+                    archivo_inst.transcription = transcripcion(archivo_inst.archivo.path)
+                else:
+                    archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
+                    archivo_inst.url = None    
                 # end
-                archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
+                #archivo_inst = Archivo.objects.create(archivo=file, nombre=file.name)
                 # resumiendo la transcripcion del archivo
                 #if archivo_inst.transcription is not None:
                 #   archivo_inst.resumen = resumen(archivo_inst.transcription)
@@ -99,10 +99,10 @@ class TransciptionApiView(APIView):
                     'nombre': archivo_inst.nombre,
                     'file_storage_name': archivo_inst.archivo.url.split('/')[-1],
                     'archivo': archivo_inst.archivo.url,
-                    'transcription': 'transcript',
+                    #'transcription': 'transcript',
                     'resumen': 'h',
                     #'keywords': 'hola',
-                    #'transcription': archivo_inst.transcription,
+                    'transcription': archivo_inst.transcription,
                     #'resumen': archivo_inst.resumen,
                     #'keywords': keywords_data,
                     #'keywords': keywords_data,
